61eba31d6563d175bcd240c2cb0a00385d6282ec
Faiz-Super <faizmhmad15@gmail.com>
2026-01-22 15:43:14 +0700

    Handle Delete Item

--- src/App.jsx
+++ src/App.jsx
@@ -29,17 +29,24 @@ export default function App() {
     setItems([...items, item]);
   }
 
+  function handleDeleteItem(id){
+    setItems((items) => items.filter((item) => item.id !== id));
+
+  }
+
   return (
   <div className="app">
     <Header />
 
     <Form onAddItem={handleAddItem} />
-    
-    <GroceryList items={items} />
+
+
+    <GroceryList items={items} onDeleteItem = {handleDeleteItem} />
 
     <Footer />
+
   </div>
-    
+
   );
 
   function Header(){
@@ -57,7 +64,6 @@ export default function App() {
       if(!name) return;
       
       const newItem = {name, quantity, checked: false,  id: Date.now()};
-
       onAddItem(newItem);
 
       console.log(newItem);
@@ -88,14 +94,14 @@ export default function App() {
   }
 }
 
-  function GroceryList({items}){
+  function GroceryList({items, onDeleteItem}){
     return (
     <>
     <div className="list">
       <ul>
         {items.map((item) => ( 
           
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem = {onDeleteItem} />
 
         ))}
       </ul>
@@ -112,14 +118,14 @@ export default function App() {
   );
 }
 
-  function Item({item}){
+  function Item({item, onDeleteItem}){
     return (
       <li key={item.id}>
             <input type="checkbox" />
             <span style= { item.checked ? { textDecoration: 'line-through' } : {}}>
               {item.quantity} {item.name}
             </span>
-            <button>&times;</button>
+            <button onClick={() =>onDeleteItem(item.id)}>&times;</button>
         </li>
     );
   }
